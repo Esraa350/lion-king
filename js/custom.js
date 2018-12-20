a454e3e69363f2171b5d4b4beb846564ce9755f1
Esraa350 <https://github.com/Esraa350>
2018-12-20 09:06:52 +0200

    first lion

---
+++ js/custom.js
@@ -0,0 +1,77 @@
+var questions=[{"question":"When was the film released?",
+               "option1":"1994",
+               "option2":"1982",
+               "option3":"1997",
+               "option4":"2001",
+               "answer":"1"
+               },{
+               "question":"What type of animal is Rafiki?",
+               "option1":"Baboon",
+               "option2":"Mandrill",
+               "option3":"Tamarin",
+               "option4":"Spider Monkey",
+               "answer":"2"
+               },{
+               "question":"What is Simba's mother's name?",
+               "option1":"Sarabi",
+               "option2":"Uru",
+               "option3":"Zuza",
+               "option4":"Shenzi",
+               "answer":"1"
+               },{
+               "question":"What is Scar doing when he first appears in the movie?",
+               "option1":"Talking to Mufasa",
+               "option2":"Attending Simba's presentation ceremony",
+               "option3":"Playing with a mouse before he eats it",
+               "option4":"Plotting with the hyenas",
+               "answer":"3"
+               },{
+               "question":"What animals do Simba and Nala ride in 'I Just Can't Wait to Be King'?",
+               "option1":"Zebras",
+               "option2":"Rhinos",
+               "option3":"Elephants",
+               "option4":"Ostriches",
+               "answer":"4"
+               },{
+               "question":"What is Nala's mother's name?",
+               "option1":"Savanna",
+               "option2":"Sarafina",
+               "option3":"Zira",
+               "option4":"Kiara",
+               "answer":"2"
+               },{
+               "question":"What area is Simba specifically forbidden from going to?",
+               "option1":"The Watering Hole",
+               "option2":"The Cheetah Hunting Ground",
+               "option3":"The Elephant Graveyard",
+               "option4":"Rafiki's Tree",
+               "answer":"3"
+               },{
+               "question":"What are Mufasa's last words?",
+               "option1":"'Run, Simba! Run!'",
+               "option2":"'You must!'",
+               "option3":"'Scar, Brother, help me!'",
+               "option4":"'Avenge me, my son.'",
+               "answer":"3"
+               },{
+               "question":"What does Hakuna Matata mean?",
+               "option1":"Return home",
+               "option2":"Family first",
+               "option3":"No worries",
+               "option4":"Just relax",
+               "answer":"3"
+               },{
+               "question":"Which character speaks to Simba right before he becomes king?",
+               "option1":"Nobody",
+               "option2":"Rafiki",
+               "option3":"Nala",
+               "option4":"Zazu",
+               "answer":"2"
+               },{
+               "question":"Who is the last new character introduced in the film?",
+               "option1":"Simba and Nala's daughter",
+               "option2":"SImba's brother",
+               "option3":"Nala's brother",
+               "option4":"Simba and Nala's son",
+               "answer":"1"
+               }];
